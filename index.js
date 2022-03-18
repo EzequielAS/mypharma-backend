a4c340589beed8cfe4c74f74f8d218954ba15758
ezequiel <ezequiel.alves@souunit.com.br>
2022-03-17 23:18:11 -0300

    new adjusts

--- index.js
+++ index.js
@@ -9,10 +9,28 @@ const ProductRoute = require('./src/routes/ProductRoute')
 const app = express()
 const port = process.env.PORT || 3000
 
+const whitelist = ['https://mypharma-frontend.vercel.app/', 'https://www.mypharma-frontend.vercel.app/']
+
+const corsOptions = {
+   origin: function(origin, callback){
+      if(!origin) 
+         return callback(null, true)
+
+      if(whitelist.indexOf(origin) === -1){
+        let msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.'
+
+        return callback(new Error(msg), false)
+      }
+
+      return callback(null, true)
+    }
+}
+
+app.use(cors(corsOptions));
+
 app.use(express.json())
-app.use(cors({
-    origin: "*"
-}))
+app.use(cors(corsOptions))
 
 app.get('/', (req, res) => {
     res.send('Hello world')   
